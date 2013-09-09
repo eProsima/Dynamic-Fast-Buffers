52f846b7d50a89817c3ecf444ecdc81b39ab60ef
Rafael Lara <rafaellara@eprosima.com>
2013-09-09 12:59:48 +0000

    - Makefile UnitTests finished

--- utils/pcTests/UnitTests/unitTests.mk
+++ utils/pcTests/UnitTests/unitTests.mk
@@ -1,3 +1,14 @@
+OUTDIR= .
+BASEDIR= ../../..
+CC=gcc
+CPP=g++
+LN=g++
+AR=ar
+CP=cp
+LNK=ln
+
+TARGET_ARCH= x64Linux2.6gcc4.7.2
+
 DFB_OUTDIR= $(OUTDIR)/unitTests
 DFB_OUTDIR_DEBUG = $(DFB_OUTDIR)/debug
 DFB_OUTDIR_RELEASE = $(DFB_OUTDIR)/release
@@ -5,32 +16,25 @@ DFB_OUTDIR_RELEASE = $(DFB_OUTDIR)/release
 DFB_SED_OUTPUT_DIR_DEBUG= $(subst /,\\/,$(DFB_OUTDIR_DEBUG))
 DFB_SED_OUTPUT_DIR_RELEASE= $(subst /,\\/,$(DFB_OUTDIR_RELEASE))
 
-DFB_TARGET_DEBUG= $(BASEDIR)/lib/$(EPROSIMA_TARGET)/libdfbd.so
-DFB_TARGET_DEBUG_Z= $(BASEDIR)/lib/$(EPROSIMA_TARGET)/libdfbzd.a
-DFB_TARGET= $(BASEDIR)/lib/$(EPROSIMA_TARGET)/libdfb.so
-DFB_TARGET_Z= $(BASEDIR)/lib/$(EPROSIMA_TARGET)/libdfbz.a
-DFB_TARGET_EXE= $(BASEDIR)/lib/$(EPROSIMA_TARGET)/unitTests.sh
+DFB_TARGET_FOLDER=  $(OUTDIR)/$(EPROSIMA_TARGET)
+DFB_TARGET_EXE= $(DFB_TARGET_FOLDER)/unitTests.sh
+
+DFB_CFLAGS += -c -Wall -O2 -m64 -fpic -std=c++11
+DFB_LDFLAGS=-m64
 
 DFB_CFLAGS += $(CFLAGS) -std=c++11
 DFB_CFLAGS_DEBUG += $(CFLAGS_DEBUG) -std=c++11
 
-DFB_LIBS_DEBUG= $(LIBS_DEBUG) -lcdrd
-DFB_LIBS= $(LIBS) -lcdr
+DFB_LIBS_DEBUG= $(LIBS_DEBUG) -lcdrd -ldfbd
+DFB_LIBS= $(LIBS) -lcdr -ldfb
 
-DFB_LIBS_DEBUG_DIR= -L$(BASEDIR)/lib/$(EPROSIMA_TARGET)
+DFB_LIBS_DEBUG_DIR= -L$(BASEDIR)/lib/$(EPROSIMA_TARGET) -L$(BASEDIR)/../CDR/lib/$(EPROSIMA_TARGET)
 DFB_LIBS_DIR= $(DFB_LIBS_DEBUG_DIR)
 
 DFB_INCLUDE_DIRS= $(INCLUDE_DIRS) -I$(BASEDIR)/include -I$(BASEDIR)/include/cpp \
-		  -I$(EPROSIMADIR)/code
+		  -I$(EPROSIMADIR)/code -I$(BASEDIR)/../CDR/include
 
-DFB_SRC_CPPFILES= $(BASEDIR)/src/cpp/TypecodeAPI.cpp \
-		  $(BASEDIR)/src/cpp/BytecodeAPI.cpp \
-		  $(BASEDIR)/src/cpp/SerializerAPI.cpp \
-  		  $(BASEDIR)/src/cpp/CommonData.cpp \
-    		  $(BASEDIR)/src/cpp/exceptions/NotEnoughParamsException.cpp \
-		  $(BASEDIR)/src/cpp/exceptions/WrongParamException.cpp \
-		  $(BASEDIR)/src/cpp/exceptions/WrongTypeException.cpp \
-		  $(BASEDIR)/utils/pcTests/UnitTests/Main.cpp
+DFB_SRC_CPPFILES= $(BASEDIR)/utils/pcTests/UnitTests/Main.cpp
 
 # Project sources are copied to the current directory
 DFB_SRCS= $(DFB_SRC_CFILES) $(DFB_SRC_CPPFILES)
@@ -39,26 +43,16 @@ DFB_SRCS= $(DFB_SRC_CFILES) $(DFB_SRC_CPPFILES)
 DFB_SOURCES_DIRS_AUX= $(foreach srcdir, $(dir $(DFB_SRCS)), $(srcdir))
 DFB_SOURCES_DIRS= $(shell echo $(DFB_SOURCES_DIRS_AUX) | tr " " "\n" | sort | uniq | tr "\n" " ")
 
-DFB_OBJS_DEBUG = $(foreach obj,$(notdir $(addsuffix .o, $(basename $(DFB_SRCS)))), $(DFB_OUTDIR_DEBUG)/$(obj))
-DFB_DEPS_DEBUG = $(foreach dep,$(notdir $(addsuffix .d, $(basename $(DFB_SRCS)))), $(DFB_OUTDIR_DEBUG)/$(dep))
-DFB_OBJS_RELEASE = $(foreach obj,$(notdir $(addsuffix .o, $(basename $(DFB_SRCS)))), $(DFB_OUTDIR_RELEASE)/$(obj))
-DFB_DEPS_RELEASE = $(foreach dep,$(notdir $(addsuffix .d, $(basename $(DFB_SRCS)))), $(DFB_OUTDIR_RELEASE)/$(dep))
 DFB_EXE_RELEASE = $(foreach obj,$(notdir $(addsuffix .o, $(basename $(DFB_SRCS)))), $(DFB_OUTDIR_RELEASE)/$(obj))
 
 OBJS+= $(DFB_OBJS_DEBUG) $(DFB_OBJS_RELEASE)
 DEPS+= $(DFB_DEPS_DEBUG) $(DFB_DEPS_RELEASE)
 
-.PHONY: utests checkDFBDirectories
-
-utests: checkDFBDirectories $(DFB_TARGET_EXE)
+all: checkDFBDirectories $(DFB_TARGET_EXE)
 
 checkDFBDirectories:
-	@mkdir -p $(OUTDIR)
-	@mkdir -p $(DFB_OUTDIR)
-	@mkdir -p $(DFB_OUTDIR_DEBUG)
 	@mkdir -p $(DFB_OUTDIR_RELEASE)
-	@mkdir -p lib
-	@mkdir -p lib/$(EPROSIMA_TARGET)
+	@mkdir -p $(DFB_TARGET_FOLDER)
 	@mkdir -p $(EPROSIMA_LIBRARY_PATH)/proyectos/$(EPROSIMA_TARGET)
 
 $(DFB_TARGET_EXE): $(DFB_EXE_RELEASE)
